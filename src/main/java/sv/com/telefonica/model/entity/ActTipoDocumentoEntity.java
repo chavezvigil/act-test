58ceacbbf387e3f25c63c9c7e18cca4cabcf63f4
Luis Josue Chavez Vigil <luis.chavez@bandesal.gob.sv>
2021-12-17 17:04:03 -0600

    Adding tipo de documento

---
+++ src/main/java/sv/com/telefonica/model/entity/ActTipoDocumentoEntity.java
@@ -0,0 +1,105 @@
+package sv.com.telefonica.model.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "act_tipo_documento")
+public class ActTipoDocumentoEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_tipo_documento", nullable = false)
+	private Integer id;
+
+	@Column(name = "nombre", nullable = false, length = 100)
+	private String nombre;
+
+	@Column(name = "mascara", nullable = false, length = 100)
+	private String mascara;
+
+	@Column(name = "creado_por", length = 100)
+	private String creadoPor;
+
+	@Column(name = "modificado_por", length = 100)
+	private String modificadoPor;
+
+	@Column(name = "fecha_creacion")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaCreado;
+
+	@Column(name = "fecha_modificacion")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaModificado;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMascara() {
+		return mascara;
+	}
+
+	public void setMascara(String mascara) {
+		this.mascara = mascara;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getModificadoPor() {
+		return modificadoPor;
+	}
+
+	public void setModificadoPor(String modificadoPor) {
+		this.modificadoPor = modificadoPor;
+	}
+
+	public Date getFechaCreado() {
+		return fechaCreado;
+	}
+
+	public void setFechaCreado(Date fechaCreado) {
+		this.fechaCreado = fechaCreado;
+	}
+
+	public Date getFechaModificado() {
+		return fechaModificado;
+	}
+
+	public void setFechaModificado(Date fechaModificado) {
+		this.fechaModificado = fechaModificado;
+	}
+
+}
---
+++ src/test/java/sv/com/telefonica/ActTipoDocumentoTests.java
@@ -0,0 +1,40 @@
+package sv.com.telefonica;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import sv.com.telefonica.model.entity.ActTipoDocumentoEntity;
+import sv.com.telefonica.model.repository.ActTipoDocumentoRepository;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class ActTipoDocumentoTests {
+
+	
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	private ActTipoDocumentoRepository repo;
+
+	@Test
+	public void testCreateUser() {
+		ActTipoDocumentoEntity tipoDocumento = new ActTipoDocumentoEntity();
+		tipoDocumento.setNombre("NIT");
+		tipoDocumento.setMascara("9999-999999-999-9");
+
+		ActTipoDocumentoEntity savedTdo = repo.save(tipoDocumento);
+		ActTipoDocumentoEntity existTdo = entityManager.find(ActTipoDocumentoEntity.class, savedTdo.getId());
+		assertThat(tipoDocumento.getNombre()).isEqualTo(existTdo.getNombre());
+
+	}
+	
+}
